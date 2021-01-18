caab74f8f7963b4e73bfa795b8ddc74e1fd208ac
Ashwika071106-rgb <72143078+Ashwika071106-rgb@users.noreply.github.com>
2021-01-18 19:46:30 +0530

    Add files via upload

--- config.js
+++ config.js
@@ -1,16 +1,26 @@
 import * as firebase from 'firebase';
+require('@firebase/firestore');
 
-require('@firebase/firestore')
 
-var firbaseConfig = {
-    apiKey: "AIzaSyBNCd9m3C8WtNdtSPCshd7w6GYcpjpRNLo",
-    authDomain: "wily-app-91d8c.firebaseapp.com",
-    projectId: "wily-app-91d8c",
-    storageBucket: "wily-app-91d8c.appspot.com",
-    messagingSenderId: "353792370638",
-    appId: "1:353792370638:web:9542a5ae6590c42b2fed9f"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
+// var firebaseConfig = {
+//   apiKey: "AIzaSyBNCd9m3C8WtNdtSPCshd7w6GYcpjpRNLo",
+//   authDomain: "wily-app-91d8c.firebaseapp.com",
+//   projectId: "wily-app-91d8c",
+//   storageBucket: "wily-app-91d8c.appspot.com",
+//   messagingSenderId: "353792370638",
+//   appId: "1:353792370638:web:9542a5ae6590c42b2fed9f"
+// };
+var firebaseConfig = {
+  apiKey: "AIzaSyBNCd9m3C8WtNdtSPCshd7w6GYcpjpRNLo",
+  authDomain: "wily-app-91d8c.firebaseapp.com",
+  projectId: "wily-app-91d8c",
+  storageBucket: "wily-app-91d8c.appspot.com",
+  messagingSenderId: "353792370638",
+  appId: "1:353792370638:web:9542a5ae6590c42b2fed9f"
+};
+// Initialize Firebase
+const firebaseApp = firebase.initializeApp(firebaseConfig);
 
-  export default firebase.firestore()
+const db = firebaseApp.firestore();
+
+export default db;
